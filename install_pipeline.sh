71aefe630762fd4636744e2b65225e84907f86ee
mmarini <Matteo_Marini@bmc.com>
2021-09-09 17:58:53 +0200

    added trigger

--- install_pipeline.sh
+++ install_pipeline.sh
@@ -9,13 +9,13 @@ kubectl apply -f skaffold-image-leeroy-web.yaml
 kubectl apply -f build-docker-image-from-git-source.yaml
 
 kubectl apply -f rbac.yaml
+kubectl apply -f tekton-ambassador.yaml
 kubectl apply -f secret.yaml
 kubectl apply -f pipeline.yaml
 kubectl apply -f eventlistener.yaml
-kubectl apply -f ingress.yaml
+#kubectl apply -f ingress.yaml
 kubectl apply -f triggertemplate.yaml
 kubectl apply -f triggerbinding.yaml
-kubectl apply -f tekton-ambassador.yaml
 
 #kubectl apply -f build-docker-image-from-git-source-task-run.yaml
 #tkn taskrun logs build-docker-image-from-git-source-task-run
