ecae3f35404208bd4cc11b85b1024b3899f577ed
mmarini <Matteo_Marini@bmc.com>
2021-09-09 12:22:10 +0200

    added trigger

--- install_pipeline.sh
+++ install_pipeline.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+kubectl apply -f https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
+kubectl apply -f https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
+kubectl apply -f https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml
+
 kubectl apply -f skaffold-git.yaml
 kubectl apply -f skaffold-image-leeroy-web.yaml
 kubectl apply -f build-docker-image-from-git-source.yaml
-kubectl apply -f build-docker-image-from-git-source-task-run.yaml
+
+kubectl apply -f rbac.yaml
+kubectl apply -f secret.yaml
+kubectl apply -f pipeline.yaml
+kubectl apply -f eventlistener.yaml
+kubectl apply -f ingress.yaml
+kubectl apply -f triggertemplate.yaml
+kubectl apply -f triggerbinding.yaml
+
+#kubectl apply -f build-docker-image-from-git-source-task-run.yaml
 #tkn taskrun logs build-docker-image-from-git-source-task-run
 
 
