ae92c6216ae3e1e9c8ea3b869d2bd6e5a5629491
mmarini <Matteo_Marini@bmc.com>
2021-09-16 17:03:08 +0200

    change to beta

--- install_pipeline.sh
+++ install_pipeline.sh
@@ -3,6 +3,8 @@
 #kubectl apply -f https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 #kubectl apply -f https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
 #kubectl apply -f https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml
+#kubectl apply --filename https://storage.googleapis.com/tekton-releases/dashboard/latest/tekton-dashboard-release.yaml
+
 
 kubectl apply -f skaffold-git.yaml
 kubectl apply -f skaffold-image-leeroy-web.yaml
